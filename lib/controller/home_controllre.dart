1d675bfd5207b4f026269383c8f89ecda94db13c
Abellatif Ben Kaida <google_ds_club@ensbiotech.edu.dz>
2022-09-11 00:24:42 +0100

    Update home_controllre.dart

--- lib/controller/home_controllre.dart
+++ lib/controller/home_controllre.dart
@@ -1,4 +1,4 @@
-   import 'package:cloud_firestore/cloud_firestore.dart';
+ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:payqr/controller/dashbord/dashboard_conroller.dart';
